597a4682610fdc2297c887893f3d86f3ca92bce2
Samir Slame <samir.slame@podilms.com>
2026-06-09 15:14:31 -0300

    ultimos cambios

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -13,8 +13,8 @@ async def main():
     try:
         async with aiomqtt.Client(
             os.environ["SERVIDOR"],
-            # username=os.environ["MQTT_USR"],
-            # password=os.environ["MQTT_PASS"],
+            username=os.environ["MQTT_USR"],
+            password=os.environ["MQTT_PASS"],
             port=int(os.environ["PUERTO_MQTTS"]),
             tls_context=tls_context,
             identifier="mi-cliente-001",
